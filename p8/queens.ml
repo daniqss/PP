066b6a1efce07a5bcc93b99e08934c07ce907fe2
ranicocs <danielqueijo14@gmail.com>
2023-12-10 21:36:19 +0100

    Add queens

--- p8/queens.ml
+++ p8/queens.ml
@@ -1,19 +1,21 @@
-let queens n = 
-    let valid_position position used =
-        let valid_position_aux position used =
-            match used with
-            | [] -> true
-            | h::t -> if fst position = fst h 
-                    || snd position = snd h
-                    || abs (fst position - fst h) = abs (snd position - snd h)
-                        then false
-                    else valid_position_aux position t
-        in valid_position_aux position used
+let valid (x1, y1) (x2, y2) =
+  x1 = x2 || y1 = y2 ||
+  abs (x2-x1) = abs (y2 - y1);;
 
-    in let rec queens_aux n used =
-        
 
-    in queens_aux n []
+let compatible p path =
+   not (List.exists (valid p) path);;
+
+   
+let queens n =
+    let rec queens_aux path i j =
+        if i > n then [path]    (* devolvemos el camino como una lista *)
+        else if j > n then []   (* devolvemos una lista vacía *)
+        else if compatible (i, j) path
+            then    (* concatenamos la solución actual con el resto de soluciones *)
+              (queens_aux ((i, j) :: path) (i+1) 1) @ (queens_aux path i (j+1))
+        else queens_aux path i (j+1)     (* seguimos buscando en la misma fila *)
+    in queens_aux [] 1 1;;
 
 
 (*
@@ -22,3 +24,7 @@ let rec is_queens_solve n l =
     | [] -> false
     | h::
 *)
+
+
+
+
