272b81fb3de791b5361c2ad4dc6ca51b649b9e05
ranicocs <danielqueijo14@gmail.com>
2023-12-10 18:08:00 +0100

    Add isort_t

--- p7/sort.ml
+++ p7/sort.ml
@@ -1,23 +1,25 @@
+(*
 let rec insert x = function
-    [] -> [x]
-    | h::t -> if x <= h then x :: h :: t
-                else h :: insert x t;;
+[] -> [x]
+| h::t -> if x <= h then x :: h :: t
+else h :: insert x t;;
 
 let rec isort = function
-    [] -> []
-    | h::t -> insert h (isort t);;
+[] -> []
+| h::t -> insert h (isort t);;
 
 let print_list lst =
   let print_element elem = Printf.printf "%d " elem in
   List.iter print_element lst;
   Printf.printf "\n"
 ;;
+*)
 
 let rec big_list n acc =
   if n = 0 then acc else big_list (n-1) (n :: acc)
 ;;
 
-(*let bigl = big_list 100000000 [];;*)
+let bigl = big_list 100000000 [];;
 
 let insert_t x l =
   let rec insert_aux acc = function
@@ -29,6 +31,19 @@ let insert_t x l =
   insert_aux [] l
 ;;
 
+let isort_t l =
+  let rec isort_aux acc = function
+    | [] -> List.rev acc
+    | h :: t -> isort_aux (insert_t h acc) t
+  in isort_aux [] l
+
+
+
+
+
+
+(*
 let lista_chica  = 11::17::7::6::5::54::3::2::4::[];;
 print_list (lista_chica);;
 print_list (insert_t  lista_chica);;
+*)
